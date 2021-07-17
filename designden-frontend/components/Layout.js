d42aa1803f30cb13e057113bcd2d272cd5bf1f25
jonah0502 <jonah0502@gmail.com>
2021-07-16 17:29:52 -0700

    working prototype

--- designden-frontend/components/Layout.js
+++ designden-frontend/components/Layout.js
@@ -2,11 +2,15 @@ import Head from 'next/head'
 import styles from '../styles/Layout.module.css'
 import Header from './Header'
 import Footer from './Footer'
-
+import Showcase from './Showcase'
+import { useRouter } from 'next/router'
 
 
 
 export default function Layout({title, keywords, description, children}) {
+    
+    const router = useRouter()
+
     return (
         <div>
             <Head>
@@ -15,6 +19,8 @@ export default function Layout({title, keywords, description, children}) {
                 <meta name='keywords' content = {keywords}/>
             </Head>
             <Header/>
+            {router.pathname === '/' && <Showcase />}
+
             <div className = {styles.container}> 
                 {children}
             </div>
---
+++ designden-frontend/components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+    return (
+        <div className={styles.showcase}>
+            <h1>Welcome to DesignDen!</h1>
+            <h2>Find the best website solution for your needs!</h2>
+        </div>
+    )
+}
