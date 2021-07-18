866486f202f60c43770203a768a261e174093e46
jonathan-nichols <jnichols@sandiego.edu>
2021-07-18 15:34:34 -0700

    Update to templates page content

--- designden-frontend/pages/templates/index.js
+++ designden-frontend/pages/templates/index.js
@@ -1,10 +1,25 @@
-import Layout from '@/components/Layout'
+import Layout from "@/components/Layout";
+import Link from "next/link";
 
-
-export default function EventsPage() {
-    return (
-        <Layout>
-            <h1>My Templates</h1>
-        </Layout>
-    )
+export default function TemplatesPage() {
+  // sample data
+  let nums = [];
+  for (let i = 0; i < 10; i++) {
+    nums[i] = i + 1;
+  }
+  // TODO: route "/templates/add" should be something like "/users/[id]/templates/add"
+  return (
+    <Layout>
+      <h1>Templates</h1>
+      <Link href="/templates/add">Post a new template</Link>
+      <div>
+        <input type="text" placeholder="Search" />
+      </div>
+      <ul>
+        {nums.map((i) => (
+          <li key={i}>Template {i}</li>
+        ))}
+      </ul>
+    </Layout>
+  );
 }
