103ca02ec383cc9bd0c2220dbdfe28a9d2177acc
jonathan-nichols <jnichols@sandiego.edu>
2021-07-18 16:52:02 -0700

    Clean up form input tags

--- designden-frontend/pages/checkout.js
+++ designden-frontend/pages/checkout.js
@@ -9,44 +9,19 @@ export default function CheckoutPage() {
       <form>
         <div>
           <h3>Contact Information</h3>
-          <input type="text" name="fname" id="fname" placeholder="First Name" />
-          <input type="text" name="lname" id="lname" placeholder="Last Name" />
-          <input type="email" name="email" id="email" placeholder="Email" />
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="email" placeholder="Email" />
         </div>
         <div>
           <h3>Billing Address</h3>
-          <input
-            type="text"
-            name="billing-street"
-            id="billing-street"
-            placeholder="Street Address"
-          />
-          <input
-            type="text"
-            name="billing-city"
-            id="billing-city"
-            placeholder="City"
-          />
-          <input
-            type="text"
-            name="billing-zip"
-            id="billing-zip"
-            placeholder="Zip"
-          />
-          <input
-            type="text"
-            name="billing-state"
-            id="billing-state"
-            placeholder="State"
-          />
-          <input
-            type="text"
-            name="billing-country"
-            id="billing-country"
-            placeholder="Country"
-          />
+          <input type="text" placeholder="Street Address" />
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="Zip" />
+          <input type="text" placeholder="State" />
+          <input type="text" placeholder="Country" />
         </div>
-        <button type="submit">Place Order</button>
+        <button>Place Order</button>
       </form>
     </Layout>
   );
--- designden-frontend/pages/login.js
+++ designden-frontend/pages/login.js
@@ -7,14 +7,9 @@ export default function LoginPage() {
     <Layout title="DesignDen Login">
       <form className={styles.container}>
         <h1>Login</h1>
-        <input type="email" id="email" name="email" placeholder="Email" />
-        <input
-          type="text"
-          id="password"
-          name="password"
-          placeholder="Password"
-        />
-        <button type="submit">Login</button>
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+        <button>Login</button>
         <div className={styles.formFooter}>
           <p>Don&apos;t have an account? </p>
           <Link href="/signup">Sign Up</Link>
