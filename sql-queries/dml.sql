e7c8746c0eac8ded3ac5391062c84cfdd6a8e50e
jonah0502 <jonah0502@gmail.com>
2021-07-24 00:09:23 -0700

    just starting dml requests

--- sql-queries/dml.sql
+++ sql-queries/dml.sql
@@ -1,2 +1,21 @@
 -- Data manipulation queries
 
+--Products
+
+--Create
+INSERT INTO products
+    (userID, description, name, imageURL, price, datePosted, lastUpdated)
+VALUES
+    (0, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra dui leo, id imperdiet eros scelerisque tempor. Etiam vitae magna vitae nulla sagittis fringilla a sed libero. Nunc nisi nulla, egestas ut fermentum placerat, volutpat ac justo. Suspendisse luctus imperdiet purus non tempor.", "Cryptocurrency Web App React JS Template", "/images/sample/template-1.jpg", 60, "2021-07-15","2021-07-15"),
+
+--Read 1
+
+SELECT * from products where productID = 1;
+
+--Read All
+SELECT * from products;
+
+--Update
+
+
+--Delete
