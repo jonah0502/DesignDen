f8bd220a2091219e3db21f5b639fb6ca394a8594
jonathan-nichols <jnichols@sandiego.edu>
2021-07-24 13:13:33 -0700

    Add select, update, and delete queries

--- sql-queries/dml.sql
+++ sql-queries/dml.sql
@@ -1,5 +1,97 @@
 -- Data manipulation queries
 
+-- USERS
+
+-- view all users
+SELECT * FROM users;
+
+-- view one user
+SELECT * FROM users
+WHERE userID = :userID;
+
+-- update a user
+UPDATE users
+SET addressID = :addressID , firstName = :firstName , lastName = :lastName , email = :email , passwordHash = :passwordHash , birthDate = :birthDate , storeCredits = :storeCredits
+WHERE userID = :userID;
+
+-- delete a user
+DELETE FROM users
+WHERE userID = :userID;
+
+
+-- REVIEWS
+
+-- view all reviews
+SELECT * FROM reviews;
+
+-- view reviews for a single product
+SELECT * FROM reviews
+WHERE productID = :productID;
+
+-- update a review
+UPDATE reviews
+SET productID = :productID , userID = :userID , stars = :stars , reviewText = :reviewText , datePosted = :datePosted , lastUpdated = :lastUpdated
+WHERE reviewID = :reviewID;
+
+-- delete a review
+DELETE FROM reviews
+WHERE reviewID = :reviewID;
+
+
+-- TAGS
+
+-- view all tags
+SELECT * FROM tags;
+
+-- update a tag
+UPDATE tags
+SET tagName = :tagName
+WHERE tagID = :tagID;
+
+-- delete a tag
+DELETE FROM tags
+WHERE tagID = :tagID;
+
+
+-- PRODUCTS_TAGS
+
+-- view all products_tags
+SELECT * FROM products_tags;
+
+-- view all tags for a single product
+SELECT tags.tagName FROM products_tags
+LEFT JOIN tags ON products_tags.tagID = tags.tagID
+WHERE productID = :productID;
+
+-- update a products_tags
+UPDATE products_tags
+SET productID = :newProductID, tagID = :newTagID
+WHERE productID = :productID AND tagID = :tagID;
+
+-- delete a products_tags
+DELETE FROM products_tags
+WHERE productID = :productID AND tagID = :tagID;
+
+
+-- USERS_PRODUCTS
+
+-- view all users_products
+SELECT * FROM users_products;
+
+-- view all products in a users carts
+SELECT * FROM users_products
+WHERE userID = :userID;
+
+-- update quantity of item in user cart
+UPDATE users_products
+SET userID = :userID, productID = :productID, quantity = :quantity
+WHERE userID = :userID AND productID = :productID;
+
+-- delete an item from user cart
+DELETE FROM users_products
+WHERE userID = :userID AND productID = :productID;
+
+
 --Products
 
 --Create
