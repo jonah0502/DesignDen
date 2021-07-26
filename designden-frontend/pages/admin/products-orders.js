00527d12cf54daf10472c160f39114e031a3efdc
jonathan-nichols <jnichols@sandiego.edu>
2021-07-25 23:49:25 -0700

    Add form to products_orders page

--- designden-frontend/pages/admin/products-orders.js
+++ designden-frontend/pages/admin/products-orders.js
@@ -5,33 +5,33 @@ const headers = ["OrderID", "ProductID", "Quantity"];
 
 const data = [
   {
-    orderID: 0,
-    productID: 1,
-    quantity: 1,
-  },
-  {
-    orderID: 0,
+    orderID: 1,
     productID: 2,
     quantity: 1,
   },
   {
     orderID: 1,
-    productID: 0,
-    quantity: 3,
-  },
-  {
-    orderID: 2,
-    productID: 1,
-    quantity: 1,
-  },
-  {
-    orderID: 2,
     productID: 3,
     quantity: 1,
   },
   {
     orderID: 2,
+    productID: 1,
+    quantity: 3,
+  },
+  {
+    orderID: 3,
+    productID: 2,
+    quantity: 1,
+  },
+  {
+    orderID: 3,
     productID: 4,
+    quantity: 1,
+  },
+  {
+    orderID: 3,
+    productID: 5,
     quantity: 1,
   },
 ];
@@ -60,6 +60,18 @@ export default function ProductOrdersPage() {
           </tbody>
         </table>
       </div>
+      <br />
+      <h3>Add a product to an order</h3>
+      <form className={styles.formContainer}>
+        <div className={styles.inputContainer}>
+          <input type="text" placeholder="OrderID" />
+          <input type="text" placeholder="ProductID" />
+          <input type="number" min="0" placeholder="Quantity" />
+        </div>
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
     </Layout>
   );
 }
