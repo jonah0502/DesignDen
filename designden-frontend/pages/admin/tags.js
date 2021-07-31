a95c036b147980ec725cbda09a0b605bf1d9fac2
jonathan-nichols <jnichols@sandiego.edu>
2021-07-31 00:34:27 -0700

    Add requests to tags admin page

--- designden-frontend/pages/admin/tags.js
+++ designden-frontend/pages/admin/tags.js
@@ -1,61 +1,60 @@
 import Layout from "@/components/Layout";
 import styles from "@/styles/Table.module.css";
-import { useState } from "react";
+import tagService from "../../services/tags.js";
+import { useEffect, useState } from "react";
 
 const tagHeaders = ["TagID", "TagName", "Update", "Delete"];
 
-const tagData = [
-  {
-    id: 1,
-    tagName: "HTML",
-  },
-  {
-    id: 2,
-    tagName: "CSS",
-  },
-  {
-    id: 3,
-    tagName: "Wordpress",
-  },
-  {
-    id: 4,
-    tagName: "React",
-  },
-  {
-    id: 5,
-    tagName: "Angular",
-  },
-  {
-    id: 6,
-    tagName: "Vue",
-  },
-  {
-    id: 7,
-    tagName: "JQuery",
-  },
-];
-
 export default function TagsPage() {
-  const [tags, setTags] = useState(tagData);
+  const [tags, setTags] = useState([]);
   const [tagInput, setTagInput] = useState("");
 
-  const handleRowAdd = (event) => {
+  useEffect(() => {
+    tagService
+      .getAll()
+      .then((response) => {
+        setTags(response);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  const addTag = (event) => {
     event.preventDefault();
-    const newID = Math.max(...tags.map((tag) => tag.id)) + 1;
-    console.log(newID);
-    setTags(tags.concat({ id: newID, tagName: tagInput }));
+    tagService
+      .create({ tagName: tagInput })
+      .then((response) => {
+        setTags(tags.concat({ tagID: response.id, tagName: tagInput }));
+        setTagInput("");
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const updateTag = (index) => (event) => {
+    event.preventDefault();
+    tagService
+      .update(tags[index].tagID, tags[index])
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const deleteTag = (index) => (event) => {
+    event.preventDefault();
+    tagService
+      .remove(tags[index].tagID)
+      .then(() => {
+        const newTags = [...tags];
+        newTags.splice(index, 1);
+        setTags(newTags);
+      })
+      .catch((e) => console.log(e));
   };
 
   const handleRowChange = (index) => (event) => {
     const { name, value } = event.target;
     const newTags = [...tags];
     newTags[index][name] = value;
-    setTags(newTags);
-  };
-
-  const handleRowDelete = (index) => (event) => {
-    const newTags = [...tags];
-    newTags.splice(index, 1);
     setTags(newTags);
   };
 
@@ -79,8 +78,8 @@ export default function TagsPage() {
           </thead>
           <tbody>
             {tags.map((tag, index) => (
-              <tr key={tag.id}>
-                <td>{tag.id}</td>
+              <tr key={tag.tagID}>
+                <td>{tag.tagID}</td>
                 <td>
                   <input
                     type="text"
@@ -90,10 +89,10 @@ export default function TagsPage() {
                   />
                 </td>
                 <td>
-                  <button>Update</button>
+                  <button onClick={updateTag(index)}>Update</button>
                 </td>
                 <td>
-                  <button onClick={handleRowDelete(index)}>Delete</button>
+                  <button onClick={deleteTag(index)}>Delete</button>
                 </td>
               </tr>
             ))}
@@ -101,7 +100,7 @@ export default function TagsPage() {
         </table>
       </div>
       <br />
-      <form className={styles.formContainer} onSubmit={handleRowAdd}>
+      <form className={styles.formContainer}>
         <h3>Add a new tag</h3>
         <input
           type="text"
@@ -109,7 +108,7 @@ export default function TagsPage() {
           value={tagInput}
           onChange={handleTagChange}
         />
-        <button type="submit">Add</button>
+        <button onClick={addTag}>Add</button>
       </form>
     </Layout>
   );
