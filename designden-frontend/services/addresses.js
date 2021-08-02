fa5d50720b6640276c2adca94e2ff0bd3e767eae
jonathan-nichols <jnichols@sandiego.edu>
2021-08-01 20:03:45 -0700

    Update to address base url and message display

--- designden-frontend/services/addresses.js
+++ designden-frontend/services/addresses.js
@@ -3,49 +3,47 @@ const axios = require("axios");
 // provides an interface to make requests to the backend
 
 // local
- const baseUrl = "http://localhost:5000";
+// const baseUrl = "http://localhost:5000/addresses/";
 
 // production
-//const baseUrl = "https://design-den-backend.herokuapp.com";
+const baseUrl = "https://design-den-backend.herokuapp.com/addresses/";
 
 //read one address
 const getOne = async (id) => {
-    const response = await axios.get(`${baseUrl}/address/${id}`);
-    return response.data;
-  };
+  const response = await axios.get(`${baseUrl}${id}`);
+  return response.data;
+};
 
 // read all addresses
 const getAll = async () => {
-  const response = await axios.get(`${baseUrl}/addresses`);
+  const response = await axios.get(baseUrl);
   return response.data;
 };
 
 // update an existing address
 const update = async (id, newAddress) => {
-    const response = await axios.put(`${baseUrl}/address/${id}`, newAddress);
-    return response.data;
-  };
-
-// delete an existing adress
-const remove = async (id) => {
-    const response = await axios.delete(`${baseUrl}/address/${id}`);
-    return response.data;
-  };
-  
-// create a new order
-const create = async (newAddress) => {
-  const response = await axios.post(`${baseUrl}/addresses`, newAddress);
+  const response = await axios.put(`${baseUrl}${id}`, newAddress);
   return response.data;
 };
 
-  
+// delete an existing adress
+const remove = async (id) => {
+  const response = await axios.delete(`${baseUrl}${id}`);
+  return response.data;
+};
+
+// create a new order
+const create = async (newAddress) => {
+  const response = await axios.post(`${baseUrl}`, newAddress);
+  return response.data;
+};
 
 const userService = {
-    getOne,
-    getAll,
-    update,
-    remove,
-    create
+  getOne,
+  getAll,
+  update,
+  remove,
+  create,
 };
 
 export default userService;
